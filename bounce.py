824a6733c1f7eceb7d518621ce3e9ce9a869e1d4
Harper Reed <harper@nata2.org>
2023-02-11 22:20:31 -0600

    i think fixed stats metrics

--- bounce.py
+++ bounce.py
@@ -1,16 +1,13 @@
 from flask import Flask, request, redirect, render_template
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-import validators
 from prometheus_client import make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import tldextract
+import validators
 
-domains_stat_name = "domain_visit"
-domains_stat_description = "visits to domains"
-domains_count = Counter(domains_stat_name, domains_stat_description)
+domain_counter_stat = Counter('counts', 'visits to domain', ['domain'])
 
 app = Flask(__name__)
 
-# Add prometheus wsgi middleware to route /metrics requests
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
     '/metrics': make_wsgi_app()
 })
@@ -22,18 +19,9 @@ def bounce():
   if validators.domain(domain):
     extracted = tldextract.extract(domain)
     domain = extracted.domain + "." + extracted.suffix
-
-    stat_name = "domain_"+domain.replace(".","_")
-    stat_description = "visits to domain: " + domain
-    domain_count = Counter(stat_name, stat_description)
-    domain_count.inc()
-    domains_count.inc()
+    domain_counter_stat.labels(domain).inc()
   else:
-    stat_name = "root_request"
-    stat_description = "request of root with no domain"
-    root_count = Counter(stat_name, stat_description)
-    root_count.inc()
-    # return render_template('landing.html')
+    domain_counter_stat.labels("root").inc()
     redirect_url = "http://harperrules.com/"
     
   return redirect(redirect_url, code=301)
