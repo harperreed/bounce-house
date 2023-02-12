bb8528a5a2a584a35d62097f5d11fc8f466293f5
Harper Reed <harper@nata2.org>
2023-02-11 22:30:12 -0600

    genericized a few things

--- bounce.py
+++ bounce.py
@@ -4,7 +4,13 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import tldextract
 import validators
 
-domain_counter_stat = Counter('counts', 'visits to domain', ['domain'])
+stat_name = "domain_counter"
+stat_desc = "visits to domain"
+
+bounce_redirect_url = "http://harperrules.com/domain/?domain="
+bounce_url = "http://harperrules.com/"
+
+domain_counter_stat = Counter(stat_name, stat_desc, ['domain'])
 
 app = Flask(__name__)
 
@@ -15,14 +21,14 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 @app.route('/')
 def bounce():
   domain = request.headers['Host']
-  redirect_url = "http://harperrules.com/domain/?domain=" + domain
+  redirect_url = bounce_redirect_url + domain
   if validators.domain(domain):
     extracted = tldextract.extract(domain)
     domain = extracted.domain + "." + extracted.suffix
     domain_counter_stat.labels(domain).inc()
   else:
     domain_counter_stat.labels("root").inc()
-    redirect_url = "http://harperrules.com/"
+    redirect_url = bounce_url
     
   return redirect(redirect_url, code=301)
   
