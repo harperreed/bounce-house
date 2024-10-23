2a23455be281852de550a67743f395597f65e96b
Harper Reed <harper@nata2.org>
2024-10-22 21:40:57 -0500

    Update test for valid domain redirection
    * Change the test to use 'x.com' instead of 'example.com'
    * Update the expected redirection location to 'http://harperrules.com/domain/?domain=x.com'

--- test_bounce.py
+++ test_bounce.py
@@ -9,9 +9,9 @@ def client():
 
 
 def test_valid_domain_redirection(client):
-    response = client.get('/', headers={'Host': 'example.com'})
+    response = client.get('/', headers={'Host': 'x.com'})
     assert response.status_code == 301
-    assert response.headers['Location'] == 'http://harperrules.com/domain/?domain=example.com'
+    assert response.headers['Location'] == 'http://harperrules.com/domain/?domain=x.com'
 
 
 def test_invalid_domain_redirection(client):
